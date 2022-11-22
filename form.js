569bf8c024e7282d75a2af4f264d1143736e7b5c
Kevin Orozco <107415092+kevndev09@users.noreply.github.com>
2022-11-22 09:04:28 -0500

    initial commit

---
+++ form.js
@@ -0,0 +1,8 @@
+function mostrarAlert() {
+    let name = document.getElementById('name').value
+        if(name == false){
+            alert('Eres acaso un usuario fantasma??');
+        }else{
+            alert('hola '+name+' se ha registrado tu entrada a esta web');
+        }
+    }
